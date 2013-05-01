a2848671e7fd6cb302c45c4f0732f396c7cf6e0f
bjartwolf <bjartnes@gmail.com>
2013-05-01 23:20:13 +0200

    almost there

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
-// Creating or opening the database
-var levelup = require('levelup');
-var db = levelup('./navdataDB', {valueEncoding: "json"});
+var express = require('express');
+var app = express();
+var jsonStream = require('JSONStream');
 
-// Creating a readable stream from the ar-drone
 var stream = require('stream');
 var arDrone = require('ar-drone');
 var client = arDrone.createClient();
@@ -14,29 +13,40 @@ client.on('navdata', function (chunk) {
     navDataStream.push({key: Date.now(), value: chunk});
 });
     
-navDataStream.pipe(db.createWriteStream());
-
-setInterval(function () {
-     client.emit('navdata', {height: 1000});
-}, 100);
-
-// adding express (considering append only code presentation style)
-var express = require('express');
-var app = express();
-var jsonStream = require('JSONStream');
-
-app.get('/historical', function(req, res){
-  var stringify = new jsonStream.stringify();
-  db.createReadStream({end: Date.now()}).pipe(stringify).pipe(res); 
-});
-
-app.listen(3000);
-
-
 // serving real-time data (maybe this should come first. is simpler)
 app.get('/rt', function(req, res){
   var stringify = new jsonStream.stringify();
   navDataStream.pipe(stringify).pipe(res); 
 });
 
+app.listen(3000);
+
+// sending data when you have no chopper
+setInterval(function () {
+     client.emit('navdata', {height: 1000});
+}, 100);
+
+// Creating or opening the database
+var levelup = require('levelup');
+var db = levelup('./navdataDB', {valueEncoding: "json"});
+
+navDataStream.pipe(db.createWriteStream());
+
+app.get('/historical', function(req, res){
+  var stringify = new jsonStream.stringify();
+  db.createReadStream({end: Date.now()}).pipe(stringify).pipe(res); 
+});
+
+
+
+// Now real challenge - serve historical AND realtime data
+app.get('/oldAndFuture', function (req, res) {
+  var stringify = new jsonStream.stringify();
+  var dbStream = db.createReadStream({end: Date.now()});
+  dbStream.pipe(stringify).pipe(res, {end: false}); 
+  dbStream.on('end', function () { 
+    //might not be perfect just yet... Need to pipe it somewhere first where it can buffer
+    navDataStream.pipe(stringify).pipe(res); 
+  });
+});
 
