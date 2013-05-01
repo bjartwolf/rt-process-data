e3e93261850966ef595a414d670be632c68f6708
bjartwolf <bjartnes@gmail.com>
2013-05-01 23:33:47 +0200

    working...

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+app.listen(3000);
+
 var jsonStream = require('JSONStream');
 
 var stream = require('stream');
@@ -15,11 +17,10 @@ client.on('navdata', function (chunk) {
     
 // Server real-time data from the helicopter, never-ending stream
 app.get('/rt', function(req, res){
-  var stringify = new jsonStream.stringify();
+  var stringify = new jsonStream.stringify(false);
   navDataStream.pipe(stringify).pipe(res); 
 });
 
-app.listen(3000);
 
 // sending data when you have no chopper
 var counter = 1000;
@@ -35,14 +36,14 @@ var db = levelup('./navdataDB', {valueEncoding: "json"});
 navDataStream.pipe(db.createWriteStream());
 
 app.get('/historical', function(req, res){
-  var stringify = new jsonStream.stringify();
+  var stringify = new jsonStream.stringify(false);
   var dbStream = db.createReadStream();
   dbStream.pipe(stringify).pipe(res); 
 });
 
 // Now real challenge - serve historical AND realtime data
 app.get('/oldAndFuture', function (req, res) {
-  var stringify = new jsonStream.stringify();
+  var stringify = new jsonStream.stringify(false);
   var dbStream = db.createReadStream();
   dbStream.pipe(stringify).pipe(res, {end: false}); // Do not emit end, http stream will close  
   dbStream.on('end', function () { // Rather, on end, start piping the real-time data
