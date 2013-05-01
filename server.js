b9e5504080d150175a57fbed9fe94f9d9ab9f96b
bjartwolf <bjartnes@gmail.com>
2013-05-01 22:22:04 +0200

    piping data out

--- server.js
+++ server.js
@@ -7,16 +7,29 @@ var stream = require('stream');
 var arDrone = require('ar-drone');
 var client = arDrone.createClient();
 
-var navDataStream = new stream.Readable(); 
+var navDataStream = new stream.Readable({objectMode: true}); 
 // It's no use asking me
 navDataStream._read = function () {};
 client.on('navdata', function (chunk) {
-    navDataStream.push(chunk);
+    navDataStream.push({key: Date.now(), value: chunk});
 });
     
 navDataStream.pipe(db.createWriteStream());
-navDataStream.pipe(process.stdout);
+//navDataStream.pipe(process.stdout);
 
 setInterval(function () {
      client.emit('navdata', '1');
 }, 1000);
+
+// adding express (considering append only code presentation style)
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+  res.write('here comes the data');
+//  db.createReadStream().pipe(res); // need to json serialize for http
+  db.createKeyStream().pipe(res);
+});
+
+app.listen(3000);
+
