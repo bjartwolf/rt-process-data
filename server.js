1d1bb7de4d7fba1b92699c9b3cbc80e7dd792705
bjartwolf <bjartnes@gmail.com>
2013-05-01 21:46:27 +0200

    streaming from drone to file

--- server.js
+++ server.js
@@ -1,1 +1,20 @@
-// This will be nice
+// Creating or opening the database
+var levelup = require('levelup');
+var db = levelup('./navdataDB', {valueEncoding: "json"});
+
+// Creating a readable stream from the ar-drone
+var stream = require('stream');
+var arDrone = require('ar-drone');
+var client = arDrone.createClient();
+NavDataStream.prototype = Object.create(stream.Read.prototype, {
+  constructor: { value: NavDataStream}}
+});
+function NavDataStream() {
+    stream.Readable.call(this, {objectMode: true});
+} 
+var navDataStream = new NavDataStream(); 
+client.on('navdata', function (data) {
+    navDataStream.push(data);
+});
+    
+navDataStream(db.createWriteStream()).
